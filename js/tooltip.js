c667e75324f427edbecbd8d37c8a26240d000c28
mengzhey <106492397+mengzhey@users.noreply.github.com>
2022-11-27 23:00:15 -0500

    11/27

--- js/tooltip.js
+++ js/tooltip.js
@@ -1,18 +1,38 @@
 tippy('#tip-image-1', {
-    delay: 200,
+    delay: 100,
     content: '<img id="icon-tip" src="asset/chef.png" style="width:60px;"><p style="font-size:20px;line-height:30px;height:285px;">Use aluminum foil: Your oven may be a bit too small for this cake pan. You need at least some space between the cake pan top and the oven. To fix this problem, you can cover the cake with aluminum foil over the top to prevent further browning once the cake reaches a nice golden color.</p>',
     theme: 'tomato',
-    animation: 'scale-subtle',
+    animation: 'shift-toward',
     placement: 'right',
     interactive: true,
     allowHTML: true,
   });
 
   tippy('#tip-image-2', {
-    delay: 200,
+    delay: 100,
     content: '<img id="icon-tip" src="asset/chef.png" style="width:60px;"><p style="font-size:20px;line-height:30px;height:285px;">Use aluminum foil: Your oven may be a bit too small for this cake pan. You need at least some space between the cake pan top and the oven. To fix this problem, you can cover the cake with aluminum foil over the top to prevent further browning once the cake reaches a nice golden color.</p>',
     theme: 'tomato',
-    animation: 'scale-subtle',
+    animation: 'shift-toward',
+    placement: 'right',
+    interactive: true,
+    allowHTML: true,
+  });
+
+  tippy('#tip-image-3', {
+    delay: 100,
+    content: '<img id="icon-tip" src="asset/chef.png" style="width:60px;"><p style="font-size:20px;line-height:30px;height:285px;">Use aluminum foil: Your oven may be a bit too small for this cake pan. You need at least some space between the cake pan top and the oven. To fix this problem, you can cover the cake with aluminum foil over the top to prevent further browning once the cake reaches a nice golden color.</p>',
+    theme: 'tomato',
+    animation: 'shift-toward',
+    placement: 'right',
+    interactive: true,
+    allowHTML: true,
+  });
+
+  tippy('#tip-image-4', {
+    delay: 100,
+    content: '<img id="icon-tip" src="asset/chef.png" style="width:60px;"><p style="font-size:20px;line-height:30px;height:285px;">Use aluminum foil: Your oven may be a bit too small for this cake pan. You need at least some space between the cake pan top and the oven. To fix this problem, you can cover the cake with aluminum foil over the top to prevent further browning once the cake reaches a nice golden color.</p>',
+    theme: 'tomato',
+    animation: 'shift-toward',
     placement: 'right',
     interactive: true,
     allowHTML: true,
