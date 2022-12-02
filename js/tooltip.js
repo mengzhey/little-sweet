a6942bd246d48eb88b05525c1e68eccd3d388b8b
mengzhey <106492397+mengzhey@users.noreply.github.com>
2022-12-01 22:47:01 -0500

    12/01

--- js/tooltip.js
+++ js/tooltip.js
@@ -1,6 +1,6 @@
 tippy('#tip-image-1', {
-    delay: 800,
-    content: '<img id="icon-tip" src="asset/chef.png" style="width:60px;"><p style="font-size:14px;line-height:20px;height:200px;">Use aluminum foil: Your oven may be a bit too small for this cake pan. You need at least some space between the cake pan top and the oven. To fix this problem, you can cover the cake with aluminum foil over the top to prevent further browning once the cake reaches a nice golden color.</p>',
+    delay: 200,
+    content: '<img id="icon-tip" src="asset/chef.png" style="width:60px;"><p style="font-size:14px;line-height:20px;height:210px;width:200px;">Use aluminum foil: Your oven may be a bit too small for this cake pan. You need at least some space between the cake pan top and the oven. To fix this problem, you can cover the cake with aluminum foil over the top to prevent further browning once the cake reaches a nice golden color.</p>',
     theme: 'tomato',
     animation: 'shift-toward',
     placement: 'right',
@@ -9,8 +9,8 @@ tippy('#tip-image-1', {
   });
 
   tippy('#tip-image-2', {
-    delay: 800,
-    content: '<img id="icon-tip" src="asset/chef.png" style="width:60px;"><p style="font-size:14px;line-height:20px;height:200px;">Use aluminum foil: Your oven may be a bit too small for this cake pan. You need at least some space between the cake pan top and the oven. To fix this problem, you can cover the cake with aluminum foil over the top to prevent further browning once the cake reaches a nice golden color.</p>',
+    delay: 200,
+    content: '<img id="icon-tip" src="asset/chef.png" style="width:60px;"><p style="font-size:14px;line-height:20px;height:210px;width:200px;">Use aluminum foil: Your oven may be a bit too small for this cake pan. You need at least some space between the cake pan top and the oven. To fix this problem, you can cover the cake with aluminum foil over the top to prevent further browning once the cake reaches a nice golden color.</p>',
     theme: 'tomato',
     animation: 'shift-toward',
     placement: 'right',
@@ -19,8 +19,8 @@ tippy('#tip-image-1', {
   });
 
   tippy('#tip-image-3', {
-    delay: 800,
-    content: '<img id="icon-tip" src="asset/chef.png" style="width:60px;"><p style="font-size:14px;line-height:20px;height:200px;">Use aluminum foil: Your oven may be a bit too small for this cake pan. You need at least some space between the cake pan top and the oven. To fix this problem, you can cover the cake with aluminum foil over the top to prevent further browning once the cake reaches a nice golden color.</p>',
+    delay: 200,
+    content: '<img id="icon-tip" src="asset/chef.png" style="width:60px;"><p style="font-size:14px;line-height:20px;height:210px;width:200px;">Use aluminum foil: Your oven may be a bit too small for this cake pan. You need at least some space between the cake pan top and the oven. To fix this problem, you can cover the cake with aluminum foil over the top to prevent further browning once the cake reaches a nice golden color.</p>',
     theme: 'tomato',
     animation: 'shift-toward',
     placement: 'right',
@@ -29,8 +29,8 @@ tippy('#tip-image-1', {
   });
 
   tippy('#tip-image-4', {
-    delay: 800,
-    content: '<img id="icon-tip" src="asset/chef.png" style="width:60px;"><p style="font-size:14px;line-height:20px;height:200px;">Use aluminum foil: Your oven may be a bit too small for this cake pan. You need at least some space between the cake pan top and the oven. To fix this problem, you can cover the cake with aluminum foil over the top to prevent further browning once the cake reaches a nice golden color.</p>',
+    delay: 200,
+    content: '<img id="icon-tip" src="asset/chef.png" style="width:60px;"><p style="font-size:14px;line-height:20px;height:210px;width:200px;">Use aluminum foil: Your oven may be a bit too small for this cake pan. You need at least some space between the cake pan top and the oven. To fix this problem, you can cover the cake with aluminum foil over the top to prevent further browning once the cake reaches a nice golden color.</p>',
     theme: 'tomato',
     animation: 'shift-toward',
     placement: 'right',
